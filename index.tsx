184e83444a6afb18016cec0f3e49d243b69ff8ce
charleskcoffie-a11y <charleskcoffie@gmail.com>
2025-12-22 12:58:28 -0500

    fix toast

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import AppWithToasts from './App';
+import { AppWithToasts } from './App';
 
 // --- Global Failsafe Error Handler ---
 const handleError = (errorEvent: ErrorEvent | PromiseRejectionEvent | { error: any }) => {
